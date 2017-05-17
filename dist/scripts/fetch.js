fbc7fa9639a5f962e95a37659a81d085234ade4f
luzhenxing <luzhenxing_1230@163.com>
2017-05-17 13:51:28 +0800

    commit

--- dist/scripts/fetch.js
+++ dist/scripts/fetch.js
@@ -10,7 +10,7 @@ define(['scripts/ajax'], function (ajax) {
     mergeParagraphs: origin + '/api/v1/projects/' + window.PID + '/merge-paragraphs',
     revises: origin + '/api/v1/revises',
     mergeRevises: origin + '/api/v1/merge-revises',
-    parasRevises: origin + '/api/v1/paras-revises',
+    parasRevises: origin + '/api/v1/paras-revises/temp',
     submitRevises: origin + '/api/v1/submit-revises',
     tempSubmitRevises: origin + '/api/v1/temp-paras-revises',
     paragraphRevises: origin + '/api/v1/paragraph-revises'
